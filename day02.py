14da2a5e136f5bb8c1062bd8993b320336adf237
12201985 <12201985@inha.edu>
2024-01-15 10:44:38 +0900

    v0.3 python day 02

---
+++ day02.py
@@ -0,0 +1,10 @@
+univ="Inha university"
+print(univ)
+print(univ[5])
+#univ[5]='U'
+#print(univ)
+subjects = ['python', 'c++', 'linux', 'data structure & algorithm' , 'database']
+print(subjects)
+print(subjects[3])
+subjects[3] = 'data structue & algorithm'
+print(subjects)
